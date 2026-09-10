44cdf6d2c6c1d94b78b262b8d68f843207343241
Serhii Pustovit <opi_test@gmail.com>
2026-09-10 13:41:47 +0300

    New feature

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+
 int main() { return 0; }
 
 float f2() { return 10; }
+
+int f3() {
+    for(int i = 0; i < 54; ++i) { std::cout << i << std::endl;}
+    int temp = 10;
+    while (temp != 10) { temp = rand() % 10; }
+}
